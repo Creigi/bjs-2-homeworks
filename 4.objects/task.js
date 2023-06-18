9e4ea035d57059b17df0a712b54583fb1edb967e
Aleksandr Baronov <creigi@yandex.ru>
2023-06-18 13:41:34 +0400

    task object done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if ("marks" in this) {
+		this.marks = [...this.marks, ...marks];
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if ("marks" in this) {
+		arrMarks = [...this.marks];
+		return avgMarks = arrMarks.reduce((acc, mark, index, arrMarks) => {
+			acc += mark;
+			if (index === arrMarks.length - 1) {
+				return acc / arrMarks.length;
+			} else {
+				return acc;
+			}
+		}, 0);
+	} else {
+		return avgMarks = 0;
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
