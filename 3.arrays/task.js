279f6a817238c06d43665c718f7ed00c7771e8b6
Aleksandr Baronov <creigi@yandex.ru>
2023-06-18 10:57:14 +0400

    task arrays done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length ? arr1.every((elem, index) => elem === arr2[index]) : false;
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return avgAge = users.filter(people => people.gender === gender).
+	map(oneGender => oneGender.age).
+	reduce((acc, item, index, arrOfAges) => {
+		acc += item;
+		if (index === arrOfAges.length - 1) {
+			return acc / arrOfAges.length;
+		}
+		return acc;
+	}, 0);
 }
